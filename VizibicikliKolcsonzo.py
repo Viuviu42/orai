8abfa35159fd8599722a60ea6b737b32f541bc09
Viuviu42 <lengyelcsongi@gmail.com>
2024-04-17 12:11:52 +0200

    5. feladat

--- VizibicikliKolcsonzo.py
+++ VizibicikliKolcsonzo.py
@@ -7,7 +7,15 @@ class Kolcsonzo:
         self.eh = eh
         self.em = em
 
-file = open("kolcsonzesek.txt", "rt")
+file = open("kolcsonzesek.txt", "rt", encoding="utf-8")
+kolcsonzesek = []
+kolcson = []
+
+
+
 for row in file:
    row = row.strip().split(";")
-   kolcson = Kolcsonzo(row[0],row[1], row[2], row[3], row[4], row[5])
+   kolcsonzesek.append(Kolcsonzo(row[0],row[1], row[2], row[3], row[4], row[5]))
+
+print("5. feladat: Napi kölcsönzések száma:", len(kolcsonzesek)-1)
+
