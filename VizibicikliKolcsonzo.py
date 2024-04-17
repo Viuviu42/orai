7766eb60c005025dc4711806590e2e7574b5dbe8
Viuviu42 <lengyelcsongi@gmail.com>
2024-04-17 12:19:28 +0200

    6. kész + véletlen str függvény

--- VizibicikliKolcsonzo.py
+++ VizibicikliKolcsonzo.py
@@ -7,9 +7,13 @@ class Kolcsonzo:
         self.eh = eh
         self.em = em
 
+    def __str__(self) -> str:
+        return self.name, self.ids
+
 file = open("kolcsonzesek.txt", "rt", encoding="utf-8")
 kolcsonzesek = []
 kolcson = []
+idopont = []
 
 
 
@@ -19,3 +23,19 @@ for row in file:
 
 print("5. feladat: Napi kölcsönzések száma:", len(kolcsonzesek)-1)
 
+inputs = input("6. feladat: Kérek egy nevet:")
+for i in kolcsonzesek:
+    if inputs == i.name:
+        idopont.append(i.sh)
+        idopont.append(i.sm)
+        idopont.append(i.eh)
+        idopont.append(i.em) 
+        kolcson.append(idopont)
+        idopont = []
+
+if len(kolcson) > 0:    
+    print(inputs, "kölcsönzései:")
+    for i in kolcson:
+        print(f"{i[0]} : {i[1]} - {i[2]} : {i[3]}")
+else:
+    print("Nem volt ilyen nevű kölcsonző!")
